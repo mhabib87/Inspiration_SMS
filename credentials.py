20f5161fc4b4152b2ae8bf24dbc17bd8e3011098
mhabib87 <maged.habib87@gmail.com>
2019-02-09 00:13:22 -0800

    Update credentials.py
    Before operation make sure to edit the credential.py with your own account_sid, auth_token, and twilio phone number, and a verified phone number.

--- credentials.py
+++ credentials.py
@@ -4,8 +4,8 @@
 # In[ ]:
 
 
-account_sid = 'AC5be49b50a3fc7b3d732520fbc10a2c17'
-auth_token = 'c8d34d216e975e3ba8f6a8f2894cd6f9'
-tw_phone = '+14846265279'
-cell_phone = '+16105707773'
+account_sid = '*******************************'
+auth_token = '********************************'
+tw_phone = '****************'
+cell_phone = '*******************'
 
